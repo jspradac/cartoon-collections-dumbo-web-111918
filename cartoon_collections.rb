147e209f073fefb49fb08f905239be14a06cb137
Samuel Prada <jspradac@gmail.com>
2018-11-06 21:37:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,8 @@ def summon_captain_planet(array)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(array)
+  
 
 def find_the_cheese# code an argument here
   # the array below is here to help
